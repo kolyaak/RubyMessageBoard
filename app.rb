a05868f4636d97f16d8ccb384fc8fbf64f2c4ef7
László Harri Németh <33941426+nlharri@users.noreply.github.com>
2018-11-28 11:42:34 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,25 +9,25 @@ set :port, 8080
 
 get '/' do
   t_msg = []
-  
+
   begin
-      # connect to the database
-      conection = PG.connect :dbname => 'messageboard', :user => 'messageboarduser', :password => 'messageboarduser'
+    # connect to the database
+    conection = PG.connect :dbname => 'messageboard', :user => 'messageboarduser', :password => 'messageboarduser'
 
-      # read data from the database
-      t_messages = conection.exec 'SELECT * FROM messageboardmessages'
+    # read data from the database
+    t_messages = conection.exec 'SELECT * FROM messageboardmessages'
 
-      # map data to t_msg, which is provided to the erb later
-      t_messages.each do |s_message|
-        t_msg.push({ nick: s_message['nickname'], msg: s_message['message'] })
-      end
+    # map data to t_msg, which is provided to the erb later
+    t_messages.each do |s_message|
+      t_msg.push({ nick: s_message['nickname'], msg: s_message['message'] })
+    end
 
   rescue PG::Error => e
-      val_error = e.message 
-      
+    val_error = e.message 
+
   ensure
-      conection.close if conection
-      
+    conection.close if conection
+ 
   end
 
   # call erb, pass parameters to it 
