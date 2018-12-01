c487089e9b4fc769586f16fa94397640a145574e
László Harri Németh <33941426+nlharri@users.noreply.github.com>
2018-12-01 09:40:28 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -32,24 +32,14 @@ get '/' do
  
   end
 
-  if params[:validationerror].to_s == "yes"
-  
-    t_val_error.unshift("Nickname and message should not be empty, and can contain only characters of the english alphabet, numbers and space.")
-    
-  end
-  
-  if params[:dberrormsg].to_s != ""
-  
-    t_val_error.unshift(params[:dberrormsg].to_s)
-    
-  end
-
   # call erb, pass parameters to it 
   erb :v_message, :layout => :l_main, :locals => {:t_msg => t_msg, :t_val_error => t_val_error}
 
 end
 
 post '/newmessage' do
+  t_msg = []
+  t_val_error = []
 
   # validate input
   val_input_regex = /^[a-zA-Z0-9 ]*$/
@@ -65,33 +55,66 @@ post '/newmessage' do
       # generate new UUID
       val_uuid = UUIDTools::UUID.random_create.to_s
   
-      # insert data into the database
+      # get time stamp
       timestamp = Time.now
+
+      # insert data into the database
       connection.exec "INSERT INTO messageboardmessages(message_id, nickname, message, timestamp) \
                        VALUES ('#{val_uuid}', '#{params[:nickname]}', '#{params[:message]}', '#{timestamp}');"
+
+      # read data from the database
+      t_messages = connection.exec 'SELECT * FROM messageboardmessages'
   
+      # map data to t_msg, which is provided to the erb later
+      t_messages.each do |s_message|
+        t_msg.unshift({ nick: s_message['nickname'], msg: s_message['message'], timestamp: s_message['timestamp'] })
+      end
+
     rescue PG::Error => e
-      val_error = e.message.to_s
-      params_for_redirect = {
-        :dberror => "yes",
-        :dberrormsg => val_error
-      }
-      query = params_for_redirect.map{|key, value| "#{key}=#{value}"}.join("&")
+      t_val_error.unshift(e.message.to_s)
 
     ensure
       connection.close if connection
    
     end
-
-    redirect to("/?#{query}")
+  
+    if t_val_error.empty?
+      # redirect to the root
+      redirect to("/")
+    else
+       # call erb, pass parameters to it 
+      erb :v_message, :layout => :l_main, :locals => {:t_msg => t_msg, :t_val_error => t_val_error}
+    end
 
   else
-    # pass the error as a parameter
-    params_for_redirect = {
-      :validationerror => "yes"
-    }
-    query = params_for_redirect.map{|key, value| "#{key}=#{value}"}.join("&")
-    redirect to("/?#{query}")
+    
+    # return error message
+    t_val_error.unshift("Nickname and message should not be empty, and can contain only characters of the english alphabet, numbers and space.")
+
+    # read the data again
+    begin
+      # connect to the database
+      connection = PG.connect :dbname => 'messageboard', :user => 'messageboarduser', :password => 'messageboarduser'
+  
+      # read data from the database
+      t_messages = connection.exec 'SELECT * FROM messageboardmessages'
+  
+      # map data to t_msg, which is provided to the erb later
+      t_messages.each do |s_message|
+        t_msg.unshift({ nick: s_message['nickname'], msg: s_message['message'], timestamp: s_message['timestamp'] })
+      end
+  
+    rescue PG::Error => e
+      t_val_error.unshift(e.message.to_s)
+  
+    ensure
+      connection.close if connection
+   
+    end
+
+    # call erb, pass parameters to it 
+    erb :v_message, :layout => :l_main, :locals => {:t_msg => t_msg, :t_val_error => t_val_error}
+
   end
 
 end
